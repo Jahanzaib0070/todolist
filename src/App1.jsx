922e152f8c804c334a0cd4672db5c921b7f78564
Jahanzaib0070 <mohammadjahanzaib007@gmail.com>
2024-07-08 15:43:52 +0500

    Update App1.jsx
    Increment and Decrement

--- src/App1.jsx
+++ src/App1.jsx
@@ -1,2 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+
+const App1 = () => {
+    const [count, setCount] = useState(0);
+    const [showModal, setShowModal] = useState(false);
+
+    const incNum = () => {
+        setCount(count + 1);
+    }
+
+    const decNum = () => {
+        count >= 1 ? setCount(count - 1) : setShowModal(true);
+    }
+
+    const closeModal = () => {
+        setShowModal(false);
+    }
+
+    return (
+        <>
+            <div className="main_div">
+                <div className="center_div">
+                    <h1>{count}</h1>
+                    <div className="buttons">
+                        <button onClick={incNum}>Increment</button>
+                        <button onClick={decNum}>Decrement</button>
+                    </div>
+                </div>
+            </div>
+            {showModal && (
+                <div className="modal">
+                    <div className="modal_content">
+                        <p>Zero limit reached</p>
+                        <button onClick={closeModal}>Close</button>
+                    </div>
+                </div>
+            )}
+        </>
+    );
+}
+
+export default App1;
 
