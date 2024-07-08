acfa7c6135ef504b7e15c676664d168f6f06dd23
Jahanzaib0070 <mohammadjahanzaib007@gmail.com>
2024-07-08 14:14:46 +0500

    Update ToDoLists.jsx

--- src/ToDoLists.jsx
+++ src/ToDoLists.jsx
@@ -1,19 +1,41 @@
 import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
+import { faTrashCan, faEdit , faSave } from "@fortawesome/free-solid-svg-icons";
 
 const ToDoLists = (props)=>{
     return(
         <>
             <div className="todo_style">
-                <li>{props.value}</li>
-                <FontAwesomeIcon 
-                className="fa-times" 
-                onClick={()=>{
-                    props.onSubmit(props.id)
-                }} 
-                icon={faTrashCan} 
-                />
+                {props.isEditing ? (
+                    <input
+                        type="text"
+                        value={props.editText}
+                        onChange={props.onEditChange} />
+                    ) : (
+                    <li>{props.value}</li>
+                )}
+
+                {props.isEditing ? (
+                    <FontAwesomeIcon
+                        className="fa-save"
+                        onClick={() => props.onSaveEdit(props.id)}
+                        icon={faSave}
+                    />
+                    ) : (
+                    <div edits>
+                        <FontAwesomeIcon
+                        className="fa-edit"
+                        onClick={() => props.onEditStart(props.id, props.value)}
+                        icon={faEdit}
+                        />
+                        <FontAwesomeIcon
+                        className="fa-times"
+                        onClick={() => {
+                            props.onSubmit(props.id);
+                        }}
+                        icon={faTrashCan} />
+                    </div>
+                )}
             </div>
         </>
     )
